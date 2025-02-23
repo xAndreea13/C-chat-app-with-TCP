0ed7d6905e3d296ba9e048cbce0b42c6f9f84968
Andreea <andreeacaragea95@gmail.com>
2025-02-23 18:21:07 +0200

    Bind server socket

--- server.c
+++ server.c
@@ -6,34 +6,66 @@
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "ws2_32.lib")
 
-#define PORT 8080  //i need more infor regarding this port
+#define PORT 8080  //need more info regarding this port
 
 int main()
 {
+    //start using windows socket dll
     WORD wVersionRequested;
     WSADATA wsaData;
-
     wVersionRequested = MAKEWORD(2, 2);
 
-    int err = WSAStartup(wVersionRequested, &wsaData);
+    int err = WSAStartup(wVersionRequested, &wsaData); 
     if(err != 0 )
     {
         printf("WSAStartup failed\n");
+        printf("Error code: %d\n", WSAGetLastError());
+        return 1;
+    }
+    else
+    {
+        printf("WSAStartup called successfully\n");
     }
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //we created a new socket
+    //create a server/listening socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //created a new socket
 
     if(sock == INVALID_SOCKET)
     {
         printf("Socket coundn't be created\n");
         printf("Error code: %d\n", WSAGetLastError());
+        return 1;
     }
     else
     {
-        printf("Socket successfully created\n");
+        printf("Socket created successfully\n");
     }
 
-    Sleep(1000);
+    //bind the socket
+    struct sockaddr_in sa_server;
+    struct hostent* local_host;
+    char * local_ip;
+
+    local_host = gethostbyname("");
+    printf("Hostname: %s\n", local_host->h_name);
+    local_ip = inet_ntoa(*(struct in_addr*)*local_host->h_addr_list);
+    printf("Local IP: %s\n", local_ip);
+
+    sa_server.sin_family = AF_INET;
+    sa_server.sin_addr.s_addr = inet_addr(local_ip);
+    sa_server.sin_port = htons(PORT);
+
+    err = bind(sock, (SOCKADDR*) &sa_server, sizeof(sa_server));
+    if(err != 0)
+    {
+        printf("Socket binded successfully\n");
+    }
+    else
+    {
+        printf("Socket binding failed\n");
+        printf("Error code: %d\n", WSAGetLastError());
+        return 1;
+    }
 
     /*
         int status = shutdown(sock, 2);
@@ -46,9 +78,9 @@ int main()
         {
             printf("Socket successfully shut down\n");
         }
-
-        WSACleanup();
     */
 
+    WSACleanup();
+   
     return 0;
 }
